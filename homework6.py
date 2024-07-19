a62a07e919208c7812ef0b7c41c46b0ba42198ec
putilov-va <putilov_69@mail.ru>
2024-07-19 15:55:56 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,19 @@
+my_dict = {'Dima': 1980, 'Petya': 2001, 'Lena': 1997, 'Sergey': 1994}
+print('Список: ', my_dict)
+print(type(my_dict))
+print("Добавленное значение: ", my_dict['Lena'])
+a = my_dict['Katya'] = 2003
+print("Удалённое значение: ", a)
+my_dict.update({'Sonya': 2009, 'Maksim': 1991})
+print('Обновлённый список: ', my_dict)
+print('В списке? : ', my_dict.get('Boris'))
+print('"Work _____________ Set"')
+my_set = {'Sonya', 'Lena', 'Roma', 'Lena', 'Roma', 2, 3, 1, 2, 3}
+print(my_set)
+print(type(my_set))
+print(my_set.add('Yana'))
+print(my_set.add('Misha'))
+print(my_set)
+print(my_set.discard(2))
+my_set.discard('Lena')
+print(my_set)
