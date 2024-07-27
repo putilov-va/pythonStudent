133e773564ab88af4c4ecf6dba54c541f3315e88
putilov-va <putilov_69@mail.ru>
2024-07-27 18:08:37 +0300

    Update module_2_3.py
    Изменил код.

--- module_2_3.py
+++ module_2_3.py
@@ -4,7 +4,8 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5] # Нужно выписы
 index = 0           # переменная
 
 while index < len(my_list):
-    if my_list[index] % 2 == 0 and my_list[index] != 0:
+        if my_list[index] > 0 and my_list[index] != 0:
         print(my_list[index])
         index += 1
-    else: index += 1
+    else:
+        index += 1
