4b22fe014953ec4ed8687e55fbbc98d5c9d54f59
putilov-va <putilov_69@mail.ru>
2024-07-27 23:09:02 +0300

    Update module_2_4.py
    Исправил код.

--- module_2_4.py
+++ module_2_4.py
@@ -3,13 +3,16 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_praimes = []
 
-for p in range(2, 16):
-    for i in range(2, p):
-        if p % i == 0:
-            not_praimes.append(p)
+for i in numbers:
+    if i == 1:
+        continue
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            not_primes.append(i)
             break
     else:
-        primes.append(p)
+        primes.append(i)
 
 print('Primes: ', primes)
 print('Not Primes: ', not_praimes)
