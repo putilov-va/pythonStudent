9a9f6480c4a2f48a18450ba18ad500e45a696024
putilov-va <putilov_69@mail.ru>
2024-08-05 22:02:04 +0300

    Add files via upload
    "Рекурсивное умножение цифр":
    Напиши функцию get_multiplied_digits, которая принимает аргумент целое число number и подсчитывает произведение цифр этого числа.
    
    Пункты задачи:
    Напишите функцию get_multiplied_digits и параметр number в ней.
    Создайте переменную str_number и запишите строковое представление(str) числа number в неё.
    Основной задачей будет отделение первой цифры в числе: создайте переменную first и запишите в неё первый символ из str_number в числовом представлении(int).
    Возвращайте значение first * get_multiplied_digits(int(str_number[1:])). Таким образом вы умножите первую цифру числа на результат работы этой же функции с числом, но уже без первой цифры.
    4 пункт можно выполнить только тогда, когда длина str_number больше 1, т.к. в противном случае не получиться взять срез str_number[1:].
    Если же длина str_number не больше 1, тогда вернуть оставшуюся цифру first.
    Стек вызовов будет выглядеть следующим образом:
    get_multiplied_digits(40203) -> 4 * get_multiplied_digits(203) -> 4 * 2 * get_multiplied_digits(3) -> 4 * 2 * 3

---
+++ module_3_5.py
@@ -0,0 +1,12 @@
+
+def get_multiplied_digits(number): # подсчитывает произведение цифр числа
+    str_number = str(number)
+    first = int(str_number[0])
+
+    if len(str_number) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+result = get_multiplied_digits(40203)
+print(result)
