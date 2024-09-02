c82a830805d18c7c3a17914cb930a8f10df21f60
putilov-va <putilov_69@mail.ru>
2024-09-03 00:58:37 +0300

    Add files via upload
    Зачем нужно наследование

---
+++ module_6_1.py
@@ -0,0 +1,52 @@
+class Animal:                    # Животные
+    alive = True                 # живой
+    fed = False                  # накормленный
+
+    def __init__(self, name):
+        self.name = name         # индивидуальное название каждого животного
+
+    def eat(self, food):         # параметр, принимающий объекты классов растений
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            Animal.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            Animal.alive = False
+
+class Mammal(Animal):           # Млекопитающее
+    pass
+
+class Predator(Animal):         # Хищник
+    pass
+
+
+class Plant:                    # Растения
+    edible = False              # съедобность
+
+    def __init__(self, name):
+        self.name = name        # индивидуальное название каждого растения
+
+class Flower(Plant):           # Цветок
+
+    def __init__(self, name):
+        self.name = name
+
+class Fruit(Plant):             # Фрукт
+    edible = True               # Живой
+
+#_________________________________________
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
