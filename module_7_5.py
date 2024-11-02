6e1a37963436e7d324a08b3a2d4b350d28189321
putilov-va <putilov_69@mail.ru>
2024-11-02 19:45:23 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,30 +1,23 @@
 import os, pathlib, time
 from pprint import pprint
 
-files = os.listdir()
-file = {this_entry for this_entry in files if not os.path.isdir(this_entry)}   # имя файла
+directory = '.'
+file = []
+for root, dirs, files in os.walk(directory):   # все файлы директории
+    for this_entry in files:
+        file.append(this_entry)
 
-pathfile = os.getcwd()                                  # путь к файлу
-filepath = os.path.join(pathfile, *file)       # полный путь к файлу
+filename = file[0]                     # имя файла
+
+pathfile = os.getcwd()                          # возвращает текущий рабочий каталог
+filepath = os.path.join(pathfile, filename)       # полный путь к файлу
 filetime = os.path.getmtime(str(filepath))               # время действия файла
 formatted_time = {time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))}
-#                                                    # отформатированное время
+#                                                    # формат и отформатированние времени
 filesize = {os.path.getsize(filepath)}         # {os.path.getsize}    # размер файла
 parent_dir = os.path.dirname(filepath)         # родительский каталог
 
-# print(file)
-# pprint(pathfile)
-# # pprint(formatted_time)
 # pprint(filepath)
-pprint(f'Обнаружен файл: {file}, Путь: {pathfile},'
+pprint(f'Обнаружен файл: {filename}, Путь: {pathfile},'
           f' Размер: {filesize} байт, Время изменения: {formatted_time},'
           f' Родительская директория: {parent_dir}')
-
-#
-# all_entries = os.listdir()
-#
-# files = {entry for entry in all_entries if not os.path.isdir(entry)}   # имя файла
-# directories = [entry for entry in all_entries if os.path.isdir(entry)]
-#
-# print('Files:', files)
-# print('Directories:', directories)
