812de072fb3f09b2f530982c0cbfa00bd1af7762
putilov-va <putilov_69@mail.ru>
2024-11-02 19:56:04 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -12,9 +12,9 @@ filename = file[0]                     # имя файла
 pathfile = os.getcwd()                          # возвращает текущий рабочий каталог
 filepath = os.path.join(pathfile, filename)       # полный путь к файлу
 filetime = os.path.getmtime(str(filepath))               # время действия файла
-formatted_time = {time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))}
+formatted_time = (time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)))
 #                                                    # формат и отформатированние времени
-filesize = {os.path.getsize(filepath)}         # {os.path.getsize}    # размер файла
+filesize = (os.path.getsize(filepath))        # {os.path.getsize}    # размер файла
 parent_dir = os.path.dirname(filepath)         # родительский каталог
 
 # pprint(filepath)
