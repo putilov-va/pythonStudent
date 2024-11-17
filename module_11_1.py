f4418a49371e72f685bb3a8017456d04c5e31189
putilov-va <putilov_69@mail.ru>
2024-11-17 09:36:19 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+article_read = pd.read_csv(
+        'https://github.com/putilov-va/pythonProject3/blob/main/kc_house_data.csv')
+# print(article_read)
+# ar = article_read.tail(15)
+# ar = article_read.describe(5)
+# ar = article_read.head(6)
+# print(ar)
