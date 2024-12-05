3b6d5625964b73fd5fadd179a40c203cfc13e73b
putilov-va <putilov_69@mail.ru>
2024-12-05 21:10:52 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,23 +1,19 @@
-import os, pathlib, time
-from pprint import pprint
+import os, time, pathlib
 
 directory = '.'
-file = []
-for root, dirs, files in os.walk(directory):   # все файлы директории
+
+for root, dirs, files in os.walk(*directory):   # все файлы директории
     for this_entry in files:
-        file.append(this_entry)
+        filename = []                            #  - это кортеж имен файлов
+        filename.append(this_entry)
+        pathfile = os.getcwd()                   # возвращает текущий рабочий каталог
+        file_path = os.path.join(pathfile, filename[0])       # полный путь к файлу
+        filetime = os.path.getmtime(str(file_path[2]))               # время действия файла
+        formatted_time = (time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)))
+        #                                               # формат и отформатированние времени
+        filesize = (os.path.getsize(file_path[2]))        # {os.path.getsize}    # размер файла
+        parent_dir = os.path.dirname(file_path)        # родительский каталог
 
-filename = file[0]                     # имя файла
-
-pathfile = os.getcwd()                          # возвращает текущий рабочий каталог
-filepath = os.path.join(pathfile, filename)       # полный путь к файлу
-filetime = os.path.getmtime(str(filepath))               # время действия файла
-formatted_time = (time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)))
-#                                                    # формат и отформатированние времени
-filesize = (os.path.getsize(filepath))        # {os.path.getsize}    # размер файла
-parent_dir = os.path.dirname(filepath)         # родительский каталог
-
-# pprint(filepath)
-pprint(f'Обнаружен файл: {filename}, Путь: {pathfile},'
-          f' Размер: {filesize} байт, Время изменения: {formatted_time},'
-          f' Родительская директория: {parent_dir}')
+        print(f'Обнаружен файл: {filename[0]}, Путь: {pathfile},'
+                  f' Размер: {filesize} байт, Время изменения: {formatted_time},'
+                  f' Родительская директория: {parent_dir}')
