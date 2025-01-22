c769f2b7548d8b8410b3f041438a5b37589d81da
putilov-va <putilov_69@mail.ru>
2025-01-22 17:50:55 +0300

    16_1

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-# "1st program"
-print(9**0.5*5)
-# "2nd program"
-print(9.99>9.98 and 1000!=1000.1)
-# "3rd program
-print(1234//10%100+5678//10%100)
-# "4th program
-print(int(42.13) == int(13.42*100)%100) or (int(13.42) == 42.13*100%100)
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+
+# Определение базового маршрута
+@app.get("/")
+async def main_page() -> dict:
+    return {'message': 'Главная страница'}
+
+'''GET-запрос — маршрут к страницe администратора'''
+@app.get('/user/admin')
+async def admin_page() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+'''GET-запрос — маршрут к страницe пользователя'''
+@app.get("/user/{user_id}")
+async def user_page(user_id: int):
+    return {'message': f"Вы вошли как пользователь № <{user_id}>"}
+
+'''GET-запрос — маршрут к страницам пользователей: Имя: <username>, Возраст: <age>.
+    вводить типа ->  user?username=Vova&age=55'''
+@app.get("/user")  #/{username}/{age}
+async def user_info(username: str, age: int) -> dict:
+    return {'messege': f"Информация о пользователе. Имя: <{username}>, Возраст: <{age}>"}
+
