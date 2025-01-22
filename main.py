a289a9a77d3ec3a35dd5015faba3d2ca1d9ca8c0
putilov-va <putilov_69@mail.ru>
2025-01-22 18:23:02 +0300

    16_2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Annotated
+from fastapi import Path
+
 
 # Создаем экземпляр приложения FastAPI
 app = FastAPI()
@@ -11,17 +14,30 @@ async def main_page() -> dict:
 
 '''GET-запрос — маршрут к страницe администратора'''
 @app.get('/user/admin')
-async def admin_page() -> dict:
+async def user_page() -> dict:
     return {'message': 'Вы вошли как администратор'}
+
+'''GET-запрос — маршрут к страницам пользователей: Имя: <username>, Возраст: <age>.
+    вводить типа ->  user?username=Vovan&age=55'''
+@app.get('/user/{username}/{age}')
+async def user_info(username: Annotated[str, Path(min_length=5,
+                                                  max_length=20,
+                                                  regex="^[A-Za-z0-9\\s]+$",
+                                                  description='Enter username',
+                                                  example='UrbanUser')],
+                    age:     Annotated[int, Path(ge=18,
+                                                 le=120,
+                                                 description="Enter age",
+                                                 example=24)]
+                    ) -> dict:
+
+    return {'messege': f"Информация о пользователе. Имя: <{username}>, Возраст: <{age}>"}
 
 '''GET-запрос — маршрут к страницe пользователя'''
 @app.get("/user/{user_id}")
-async def user_page(user_id: int):
+async def user_page(user_id: Annotated[int, Path(ge=1,
+                                                 le=100,
+                                                 description="Enter User ID",
+                                                 example=1)]
+                    ):
     return {'message': f"Вы вошли как пользователь № <{user_id}>"}
-
-'''GET-запрос — маршрут к страницам пользователей: Имя: <username>, Возраст: <age>.
-    вводить типа ->  user?username=Vova&age=55'''
-@app.get("/user")  #/{username}/{age}
-async def user_info(username: str, age: int) -> dict:
-    return {'messege': f"Информация о пользователе. Имя: <{username}>, Возраст: <{age}>"}
-
