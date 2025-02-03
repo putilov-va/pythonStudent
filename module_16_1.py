ed5f1d32fe5de477e0a8dc49adcc043b6aeec278
putilov-va <putilov_69@mail.ru>
2025-02-03 22:15:50 +0300

    Update module_16_1.py
    Исправлен возврат.

--- module_16_1.py
+++ module_16_1.py
@@ -6,22 +6,22 @@ app = FastAPI()
 
 # Определение базового маршрута
 @app.get("/")
-async def main_page() -> dict:
-    return {'message': 'Главная страница'}
+async def main_page() -> str:
+    return "Главная страница"
 
 '''GET-запрос — маршрут к страницe администратора'''
 @app.get('/user/admin')
-async def admin_page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def admin_page() -> str:
+    return "Вы вошли как администратор"
 
 '''GET-запрос — маршрут к страницe пользователя'''
 @app.get("/user/{user_id}")
-async def user_page(user_id: int):
-    return {'message': f"Вы вошли как пользователь № <{user_id}>"}
+async def user_page(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 '''GET-запрос — маршрут к страницам пользователей: Имя: <username>, Возраст: <age>.
     вводить типа ->  user?username=Vova&age=55'''
 @app.get("/user")  #/{username}/{age}
-async def user_info(username: str, age: int) -> dict:
-    return {'messege': f"Информация о пользователе. Имя: <{username}>, Возраст: <{age}>"}
+async def user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: '{username}', Возраст {age}"
 
